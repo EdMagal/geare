9eb4006c91dd32cf03163539b284619212de7fee
eduardo pimenta <ed83isme@gmail.com>
2022-11-04 23:28:14 -0300

    added a seed file to create test database automatically

--- db/schema.rb
+++ db/schema.rb
@@ -14,19 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_135919) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.boolean "admin", default: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   create_table "grus", force: :cascade do |t|
     t.integer "codigoServico"
     t.string "referencia"
@@ -49,6 +36,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_135919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_grus_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "grus", "users"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Cria três usuários, sendo que um é administrador:
+
+usersEmails = %w(admin@admin.com edu@edu.com alice@alice.com bob@bob.com)
+usersPasswords = %w(123456 123456 123456 123456)
+usersAdmin = [true, true, false, false]
+
+usersEmails.each_with_index do |email, index|
+  User.create(email: email, password: usersPasswords[index], admin: usersAdmin[index])
+end
+
+
+# Cria 5 GRU para cada usuário, exceto administrador
+5.times do
+  userId = User.all.reject(&:admin).sample.id
+  userEmail = User.find(userId).email
+
+  Gru.create(codigoServico: 841,
+    referencia: "",
+    competencia: "",
+    vencimento: "",
+    cnpjCpf: "03382821141",
+    nomeContribuinte: userEmail.split("@").first,
+    valorPrincipal: rand(10..100),
+    valorDescontos: nil,
+    valorOutrasDeducoes: nil,
+    valorMulta: nil,
+    valorJuros: nil,
+    valorOutrosAcrescimos: nil,
+    urlRetorno: nil,
+    urlNotificacao: nil,
+    modoNavegacao: nil,
+    expiracaoPix: nil,
+    status: 0,
+    user_id: userId
+  )
+
+end
